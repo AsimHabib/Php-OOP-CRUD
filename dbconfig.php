d157410bed358947601e9043004022ce2cdf7fa4
Asim Habib <asimhabib2@gmail.com>
2017-11-18 14:07:29 -0500

    config file updated

--- dbconfig.php
+++ dbconfig.php
@@ -1,15 +1,9 @@
 <?php
 // Set the credentials for database
-/*$DB_host = "localhost";
+$DB_host = "localhost";
 $DB_user = "root";
 $DB_pass = "dbpass";
 $DB_name = "db-php-oop-crud";
-*/
-$DB_host = "db709115212.db.1and1.com";
-$DB_user = "dbo709115212";
-$DB_pass = "Euro@2017";
-$DB_name = "db709115212";
-
 
 //make a new PDO Connection
 try{
@@ -21,10 +15,5 @@ catch(PDOException $e)
 {
     echo "Connection failed: " . $e->getMessage();
 }
-/*
-// include the Class file and instantiate
-include_once 'classes/class.crud.php';
-// instantiate or create Calss object
-$crud = new CrudClass($DB_con);
-*/
+
 ?>
